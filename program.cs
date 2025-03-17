bf754b35050fbde4ce093c12d49b6b7201ef4975
Craftzman-Hamza <dev.hamzasultan@gmail.com>
2025-03-17 22:17:40 +0500

    Code for Method Overriding Added

--- program.cs
+++ program.cs
@@ -1,16 +1,20 @@
+using ConsoleApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace MethodOverriding
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Cat cat = new Cat();
+            cat.eat();
+
+            Console.ReadKey();
         }
     }
 }
