63792d4f2bfb7ec2545fae802a67c25bc41963d5
Craftzman-Hamza <dev.hamzasultan@gmail.com>
2025-03-17 22:12:32 +0500

    Code for Classes and Objects Added

--- program.cs
+++ program.cs
@@ -1,16 +1,29 @@
+using HelloWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace ClassAndObjects
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Object Initialization
+            Employee employeeObj = new Employee();
+
+            //Setting Properties
+            employeeObj.Id = 1;
+            employeeObj.Name = "Hamza";
+            employeeObj.Department = "Game QA";
+
+            //Actions
+            employeeObj.CheckIn();
+            employeeObj.CheckOut();
+
+            Console.ReadKey();
         }
     }
 }
